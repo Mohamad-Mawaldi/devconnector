64df6e7dba05c1dbf037226e444c7cfdacee1ccb
Mohamad <ima.mawaldi@gmail.com>
2019-05-10 12:53:03 +0200

    added models file with user mongoose Schema

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = user = mongoose.model("user", UserSchema);
