3da44dde3d524d7a4a8c5b2241beebfb75ecb6c6
Mohamad <ima.mawaldi@gmail.com>
2019-05-10 12:21:16 +0200

    added mongoDB connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//Connect Database
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is working on port ${PORT}`));
