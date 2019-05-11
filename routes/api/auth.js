f75933cc6091efc96c5f8127e37bb0a80cba0f18
Mohamad <ima.mawaldi@gmail.com>
2019-05-11 16:17:19 +0200

    added a protected route using auth middleware and jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,20 @@
-express = require("express");
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
 //@route    GET api/auth
 //@desc     Test auth
 //@access   public
 
-router.get("/", (req, res) => res.send("Auth route Works!"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 //@route    Post api/users
 //@desc     Register User
@@ -18,13 +23,64 @@ router.post(
       "please enter a password with a 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });
     }
-    console.log("req.body", req.body);
-    res.send("User route Works!");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      // check user exists
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "user already exists" }] });
+      }
+
+      // get user Gravatar
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // get json web token (logged in after registering)
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000
+        },
+        (error, token) => {
+          if (error) throw error;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log("error", error.message);
+      res.status(500).send("server error");
+    }
   }
 );
 
