52f263ec5a25b6d8f91c4e6af692f628a4f70d2c
Mohamad <ima.mawaldi@gmail.com>
2019-05-12 15:04:04 +0200

    added Get current users profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,25 @@
 express = require("express");
 const router = express.Router();
 
-//@route    GET api/profile
-//@desc     Test route
-//@access   public
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
 
-router.get("/", (req, res) => {
-  res.send("Profile route Works!");
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
